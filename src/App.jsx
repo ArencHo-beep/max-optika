898b6d4dcbad0bcd2c2fe2396f52cc719a880e8c
Regi <regi.mele@sciant.com>
2025-10-04 15:53:17 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   return (
     <>
       <NavScroll />
+      Pershendetje
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
